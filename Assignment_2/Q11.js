5d83126429bb76efb3cbc80f6e6dbcc172188cd6
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 01:08:07 +0530

    Update Q11.js

--- Assignment_2/Q11.js
+++ Assignment_2/Q11.js
@@ -3,9 +3,6 @@ function isNumber(value) {
 }
 
 // Example usage:
-console.log(isNumber(5));          // Output: true
-console.log(isNumber("Hello"));    // Output: false
-console.log(isNumber(null));       // Output: false
-console.log(isNumber(undefined));  // Output: false
-console.log(isNumber(NaN));        // Output: false
-console.log(isNumber(10 / 0));      // Output: false (Infinity is not considered a number in this context)
+console.log("IsNumber (123):", isNumber(123));  // true
+console.log("IsNumber ('123'):", isNumber('123'));  // false
+console.log("IsNumber (NaN):", isNumber(NaN));  // false
