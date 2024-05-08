5dbc73afa2a3472e439aad22ee404306ff14cc90
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 01:10:55 +0530

    Update Q13.js

--- Assignment_2/Q13.js
+++ Assignment_2/Q13.js
@@ -1,16 +1,16 @@
-var student = {
+//13. Write a JavaScript program to delete the rollno property from
+//the following object. Also print the object before or after deleting
+//the property.
+let student = {
     name: "David Rayy",
     sclass: "VI",
     rollno: 12
 };
 
-// Print the object before deleting the property
-console.log("Object before deleting the rollno property:");
-console.log(student);
 
-// Delete the rollno property
+console.log("Before deleting 'rollno':", student);
+
 delete student.rollno;
 
-// Print the object after deleting the property
-console.log("\nObject after deleting the rollno property:");
-console.log(student);
+
+console.log("After deleting 'rollno':", student);
