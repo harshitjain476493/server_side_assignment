e58828bc9306b530773a3912c2deadb9696939fc
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 00:58:23 +0530

    Update Q1.js

--- Assignment_2/Q1.js
+++ Assignment_2/Q1.js
@@ -1,19 +1,16 @@
 // Write a JavaScript program to find all the index positions of a given word within a given string.
-function findAllOccurrences(mainString, wordToFind) {
+function findAllIndexes(mainString, subString) {
     let indexes = [];
-    let index = mainString.indexOf(wordToFind);
-
+    let index = mainString.indexOf(subString);
     while (index !== -1) {
         indexes.push(index);
-        index = mainString.indexOf(wordToFind, index + 1);
+        index = mainString.indexOf(subString, index + 1);
     }
-
     return indexes;
 }
 
 // Example usage:
-let mainString = "This is an example sentence with the word 'example' repeated multiple times. This example demonstrates how to find all occurrences of a specific word.";
-let wordToFind = "example";
-
-let occurrences = findAllOccurrences(mainString, wordToFind);
-console.log("Occurrences of '" + wordToFind + "' found at index positions: " + occurrences.join(", "));
+const mainString = "hello world hello hello";
+const subString = "hello";
+const indexes = findAllIndexes(mainString, subString);
+console.log("Indexes of '" + subString + "' in '" + mainString + "':", indexes);
