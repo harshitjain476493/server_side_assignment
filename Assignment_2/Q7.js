8b3ffb4a237054cf4bbd9fc55f7ce57f53018e15
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 01:04:18 +0530

    Update Q7.js

--- Assignment_2/Q7.js
+++ Assignment_2/Q7.js
@@ -1,23 +1,20 @@
 //Write a JavaScript program to sort a list of elements using Bubble sort
 function bubbleSort(arr) {
-    const n = arr.length;
+    let n = arr.length;
     let swapped;
-
     do {
         swapped = false;
-        for (let i = 0; i < n - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                // Swap arr[i] and arr[i + 1]
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        for (let i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+                [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
                 swapped = true;
             }
         }
+        n--;
     } while (swapped);
-
     return arr;
 }
 
-// Example usage:
-const unsortedArray = [5, 3, 8, 4, 2, 7, 1, 6];
+const unsortedArray = [7, 2, 1, 6, 8, 5, 3, 4];
 const sortedArray = bubbleSort(unsortedArray);
-console.log("Sorted array:", sortedArray);
+console.log("Sorted Array (Bubble Sort):", sortedArray);
