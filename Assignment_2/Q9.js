fe2102cd50c02323914028d773f232f9beca1c63
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 01:06:27 +0530

    Update Q9.js

--- Assignment_2/Q9.js
+++ Assignment_2/Q9.js
@@ -2,15 +2,14 @@
 function isArraySorted(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] > arr[i + 1]) {
-            return false; // If any element is greater than the next one, the array is not sorted
+            return false;
         }
     }
-    return true; // If no such element found, the array is sorted
+    return true;
 }
 
-// Example usage:
-const sortedArray = [1, 2, 3, 4, 5];
-const unsortedArray = [5, 3, 8, 4, 2, 7, 1, 6];
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [5, 1, 2, 3, 4];
 
-console.log("Is sortedArray sorted?", isArraySorted(sortedArray));
-console.log("Is unsortedArray sorted?", isArraySorted(unsortedArray));
+console.log("Is array1 sorted?", isArraySorted(array1));
+console.log("Is array2 sorted?", isArraySorted(array2));
