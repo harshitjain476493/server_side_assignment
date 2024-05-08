7808d37ee4c81e1e97466247a3dee033a245717c
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 01:06:02 +0530

    Update Q8.js

--- Assignment_2/Q8.js
+++ Assignment_2/Q8.js
@@ -1,15 +1,8 @@
 // Write a JavaScript program to sort the characters in a string alphabetically.
-function isArraySorted(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
+function sortString(str) {
+    return str.split('').sort().join('');
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [5, 1, 2, 3, 4];
-
-console.log("Is array1 sorted?", isArraySorted(array1));
-console.log("Is array2 sorted?", isArraySorted(array2));
+const inputString = "javascript";
+const sortedString = sortString(inputString);
+console.log("Sorted string:", sortedString);
