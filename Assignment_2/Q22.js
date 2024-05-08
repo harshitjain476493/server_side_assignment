8a58c6ba8c66b89710730a3a4e1f7ee8c14d9b78
Harshit Jain <130424183+harshitjain476493@users.noreply.github.com>
2024-05-09 01:54:22 +0530

    Update Q22.js

--- Assignment_2/Q22.js
+++ Assignment_2/Q22.js
@@ -16,12 +16,12 @@ class Rectangle {
 }
 
 // Create an instance of the Rectangle class
-const rectangle = new Rectangle(5, 7);
+const rectangle = new Rectangle(5, 8);
 
 // Calculate area and perimeter
 const area = rectangle.calculateArea();
 const perimeter = rectangle.calculatePerimeter();
 
 // Output the results
-console.log("Rectangle Area:", area);
-console.log("Rectangle Perimeter:", perimeter);
+console.log("Rectangle Area:", area); // output:Area of the rectangle: 40
+console.log("Rectangle Perimeter:", perimeter); // output:Perimeter of the rectangle: 26
